9ebed6bcf13fae0d26ccb9ec59635bd13ca7880e
Gabriel Miranda <gabrielcm.br@gmail.com>
2018-05-24 20:32:27 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,8 @@
 def oxford_comma(array)
-  length = array.length
-  lastword = array.pop
-  result = array.join(", ")
-  result << " and " << lastword
+  if array.length < 2
+    array.join
+  else
+    lastword = array.pop
+    result = array.join(", ")
+    result << " and #{lastword}"
 end
