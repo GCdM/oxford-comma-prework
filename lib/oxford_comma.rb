b7d50475fd6f4a28318f7ccb9922598d46a1e6b9
Gabriel Miranda <gabrielcm.br@gmail.com>
2018-05-24 20:34:06 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,8 @@
 def oxford_comma(array)
-  if array.length < 2
+  if array.length == 1
     array.join
+  elsif array.length == 2
+    array.join(" and ")
   else
     lastword = array.pop
     result = array.join(", ")
